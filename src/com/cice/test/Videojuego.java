127c9a3dadda13617793f2b4049e9b5f570773b8
Alejandro Lozano <alex93lzn@gmail.com>
2020-03-23 18:48:32 +0100

    Cambios en ejecutador y cambio nombre clase HTML

--- src/com/cice/test/Videojuego.java
+++ src/com/cice/test/Videojuego.java
@@ -6,39 +6,47 @@ public class Videojuego implements Serializable {
 
 	private String titulo;
 	private String plataforma;
-	private double Precio;
+	private double precio;
+
 	public Videojuego() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Videojuego(String titulo, String plataforma, double precio) {
 		super();
 		this.titulo = titulo;
 		this.plataforma = plataforma;
-		Precio = precio;
+		this.precio = precio;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getPlataforma() {
 		return plataforma;
 	}
+
 	public void setPlataforma(String plataforma) {
 		this.plataforma = plataforma;
 	}
+
 	public double getPrecio() {
-		return Precio;
+		return precio;
 	}
+
 	public void setPrecio(double precio) {
-		Precio = precio;
+		this.precio = precio;
 	}
+
 	@Override
 	public String toString() {
-		return "Videojuego [titulo=" + titulo + ", plataforma=" + plataforma + ", Precio=" + Precio + "]";
+		return "Videojuego [titulo=" + titulo + ", plataforma=" + plataforma + ", Precio=" + precio + "]";
 	}
-	
-	
+
 }
