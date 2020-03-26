78c8d1c1fd86465d1d62ced94cad1f8c1d17d9b2
Facundo J. López <facundjolopez@gmail.com>
2020-03-26 20:35:04 +0100

    Revert "Revert "Cambios en ejecutador y cambio nombre clase HTML""
    This reverts commit 2e61f8d3874a4d0c6f3347487cf7c0fc23b57ded.

--- src/com/cice/test/HTML_Generator.java
+++ src/com/cice/test/HTML_Generator.java
@@ -10,38 +10,39 @@ import java.io.IOException;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-public class HtmlGenarator {
+public class HTML_Generator {
 	private static BufferedReader plantilla;
 
 	public static void loadPlantilla() {
 		try {
 			plantilla = new BufferedReader(new FileReader("resource/plantilla.html"));
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public static void printHtlm(Videojuego v) {
-		String line  = "";
-		loadPlantilla();
-		//  TRY-CATCH RESURCE
-		try (BufferedWriter outputStream = new BufferedWriter(
-				new FileWriter("C:/VideJuegos/"+v.getTitulo() +".html"))){
 
-			while((line = plantilla.readLine()) !=null) {
-				if (line.contains("${Titulo}"))line=line.replace("${Titulo}", v.getTitulo());
-				if (line.contains("${Plataforma}"))line=line.replace("${Plataforma}", v.getPlataforma());
-				if (line.contains("${Precio}"))line = line.replace("${Precio}", String.valueOf(v.getPrecio()));
+	public static void printHtlm(Videojuego v) {
+		String line = "";
+		loadPlantilla();
+		// TRY-CATCH RESURCE
+		try (BufferedWriter outputStream = new BufferedWriter(
+				new FileWriter("C:/VideJuegos/" + v.getTitulo() + ".html"))) {
+
+			while ((line = plantilla.readLine()) != null) {
+				if (line.contains("${Titulo}"))
+					line = line.replace("${Titulo}", v.getTitulo());
+				if (line.contains("${Plataforma}"))
+					line = line.replace("${Plataforma}", v.getPlataforma());
+				if (line.contains("${Precio}"))
+					line = line.replace("${Precio}", String.valueOf(v.getPrecio()));
 				System.out.println(line);
 				outputStream.write(line);
 			}
 		} catch (IOException e1) {
 
 			e1.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				plantilla.close();
 			} catch (IOException e) {
@@ -50,16 +51,16 @@ public class HtmlGenarator {
 			}
 		}
 	}
-	
+
 	public static void printJSON(Videojuego v) {
-		
+
 		try (BufferedWriter outputStream = new BufferedWriter(
-				new FileWriter("C:/VideJuegos/"+v.getTitulo() +".json"))){
-				new JSONObject(v).write(outputStream);
-				
+				new FileWriter("C:/VideJuegos/" + v.getTitulo() + ".json"))) {
+			new JSONObject(v).write(outputStream);
+
 		} catch (IOException | JSONException e) {
-			
-			e.printStackTrace();	
+
+			e.printStackTrace();
 		}
 	}
 }
